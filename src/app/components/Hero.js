b14167357378e0e40beb2b1220cb080edd5615d8
wesleydematos <wesleydematos3@gmail.com>
2023-09-04 14:50:53 -0300

    feat: created location selection

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -10,7 +10,7 @@ export default function Hero() {
   return (
     <section className="h-screen xl:h-[90vh] bg-[#b2b7c2]/10" id="home">
       <div className="container mx-auto h-full xl:pt-10 ">hero container</div>
-      {/* {searchActive ? (
+      {searchActive ? (
         <div className="fixed top-[80px] z-10 w-full max-w-[1920px]">
           <Search />
         </div>
@@ -18,17 +18,7 @@ export default function Hero() {
         <div className="-mt-12 max-w[1300px] mx-auto">
           <Search />
         </div>
-      )} */}
-
-      <div
-        className={`${
-          searchActive
-            ? "fixed top-[80px] z-10 w-full max-w-[1920px]"
-            : "-mt-12 max-w[1300px] mx-auto"
-        }`}
-      >
-        <Search />
-      </div>
+      )}
     </section>
   );
 }
