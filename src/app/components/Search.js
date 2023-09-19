38d851ed91550645dc6349634f4dd693f7259f98
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-09-19 10:44:22 -0300

    feat: created HoursSelection

--- src/app/components/Search.js
+++ src/app/components/Search.js
@@ -6,6 +6,7 @@ import { useSeachContext } from "../context/search";
 //components
 import LocationSelection from "./LocationSelection";
 import DateSelection from "./DateSelection";
+import HoursSelection from "./HoursSelection";
 
 export default function Search() {
   const { searchActive } = useSeachContext();
@@ -21,6 +22,7 @@ export default function Search() {
       <div className="xl:h-full flex items-center px-6 xl:px-0">
         <LocationSelection />
         <DateSelection />
+        <HoursSelection />
       </div>
     </div>
   );
