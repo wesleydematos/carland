2377524a9b3d4f86682d26d28cd6d3438a44fef2
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-09-24 10:01:47 -0300

    feat: created base About

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -1,7 +1,58 @@
+"use client"
+
+//next image
+import Image from "next/image";
+
+//icons
+import { MdMapsHomeWork, MdOutlineBuildCircle, MdOutlineDirectionsCar } from "react-icons/md";
+
+//react count up
+import CountUp from "react-countup";
+
+//intecsection observer
+import { useInView } from "react-intersection-observer";
+
+//motion
+import { motion } from "framer-motion"; 
+
+//variants
+import { fadeIn } from "../../../variants";
+
 export default function About() {
+  const [ref, inView] = useInView({
+    threshold: 0.5
+  })
+
   return (
-    <section className="section flex items-center bg-pink-200" id="about">
-      About
+    <section 
+      className="section flex items-center" 
+      id="about"
+      ref={ref}
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:justify-between">
+          <div>
+            <Image className="rounded-[20px]" src={"/images/about/car01.png"} width={600} height={448} alt="car"/>
+          </div>
+          <div>
+            <div>
+              <h2>Car services simplified.</h2>
+              <p>Rent, choose and repair with ease. Our convenient locations,
+                diverse car types and reliable repair points ensure a seamless
+                car experience.
+              </p>
+              <div>
+                <div>
+                  <MdOutlineDirectionsCar className="text-5xl text-accent"/>
+                  <div className="text-3xl font-black mb-2">
+                    {inView ? <><CountUp start={0} end={50} duration={3} delay={1}/>+</> : null}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
