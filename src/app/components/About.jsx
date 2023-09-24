0ded19c4eb6ee07de90f51794be2ccd4bd284ead
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-09-24 20:18:35 -0300

    fix: adjusted About page

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -4,7 +4,7 @@
 import Image from "next/image";
 
 //icons
-import { MdMapsHomeWork, MdOutlineBuildCircle, MdOutlineDirectionsCar } from "react-icons/md";
+import { MdOutlineBuildCircle, MdOutlineDirectionsCar, MdOutlineMapsHomeWork } from "react-icons/md";
 
 //react count up
 import CountUp from "react-countup";
@@ -31,24 +31,83 @@ export default function About() {
     >
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row xl:justify-between">
-          <div>
+          <motion.div 
+            className="flex-1 mb-8 xl:mb-0"
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{once: false, amount: 0.6}}
+            variants={fadeIn("up", 0.2)}
+          >
             <Image className="rounded-[20px]" src={"/images/about/car01.png"} width={600} height={448} alt="car"/>
-          </div>
-          <div>
-            <div>
-              <h2>Car services simplified.</h2>
-              <p>Rent, choose and repair with ease. Our convenient locations,
+          </motion.div>
+          <div className="flex-1 flex items-center xl:justify-center">
+            <div className="xl:max-w-[517px]">
+              <motion.h2 
+                className="h2"
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{once: false, amount: 0.6}}
+                variants={fadeIn("up", 0.4)}
+              >
+                Car services simplified.
+              </motion.h2>
+              <motion.p 
+                className="mb-[42px] max-w-md"
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{once: false, amount: 0.6}}
+                variants={fadeIn("up", 0.6)}
+              >
+                Rent, choose and repair with ease. Our convenient locations,
                 diverse car types and reliable repair points ensure a seamless
                 car experience.
-              </p>
-              <div>
-                <div>
-                  <MdOutlineDirectionsCar className="text-5xl text-accent"/>
+              </motion.p>
+              <motion.div 
+                className="flex items-center gap-x-8 mb-12"
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{once: false, amount: 0.2}}
+                variants={fadeIn("up", 0.8)}
+              >
+                <div className="flex flex-col w-[100px]">
+                  <MdOutlineDirectionsCar className="text-5xl text-accent mb-2"/>
                   <div className="text-3xl font-black mb-2">
                     {inView ? <><CountUp start={0} end={50} duration={3} delay={1}/>+</> : null}
                   </div>
+                  <div className="uppercase text-[13px] font-semibold text-secondary">
+                    car <br /> types
+                  </div>
                 </div>
-              </div>
+                <div className="flex flex-col w-[100px]">
+                  <MdOutlineMapsHomeWork className="text-5xl text-accent mb-2"/>
+                  <div className="text-3xl font-black mb-2">
+                    {inView ? <><CountUp start={0} end={135} duration={3} delay={1}/>+</> : null}
+                  </div>
+                  <div className="uppercase text-[13px] font-semibold text-secondary">
+                    rental <br /> outlets
+                  </div>
+                </div>
+                <div className="flex flex-col w-[100px]">
+                  <MdOutlineBuildCircle className="text-5xl text-accent mb-2"/>
+                  <div className="text-3xl font-black mb-2">
+                    {inView ? <><CountUp start={0} end={35} duration={3} delay={1}/>+</> : null}
+                  </div>
+                  <div className="uppercase text-[13px] font-semibold text-secondary">
+                    repair <br /> points
+                  </div>
+                </div>
+              </motion.div>
+              <motion.button 
+                className="hidden xl:block bg-accent hover:bg-accent-hover
+                rounded-[10px] w-full h-16 uppercase font-medium text-white
+                tracking-[2px] text-[13px] max-w-[184px]"
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{once: false, amount: 0.6}}
+                variants={fadeIn("up", 1)}
+              >
+                See all cars
+              </motion.button>
             </div>
           </div>
         </div>
