b1165badf8ad22f01b8cf4445321f9f5a0c98e54
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-09-26 10:29:27 -0300

    feat: created back to top btn

--- src/app/components/BackToTopBtn.jsx
+++ src/app/components/BackToTopBtn.jsx
@@ -1,3 +1,39 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+//icons
+import { FaChevronUp } from "react-icons/fa";
+
+//react scroll
+import { Link } from "react-scroll";
+
 export default function BackToTopBtn() {
-  return <button>Back to top</button>;
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(()=>{
+    const handleScroll = () => {
+      if(window.scrollY > 400){
+        setIsActive(true)
+      } else {
+        setIsActive(false)
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll)
+    
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+
+  return <Link 
+    to="home"
+    smooth
+    className={`${!isActive && "hidden"} fixed bg-accent hover:bg-accent-hover w-12 h-12
+    right-16 bottom-11 z-10 cursor-pointer flex justify-center items-center text-white
+    border-2 border-white`}
+  >
+    <FaChevronUp className="text-xl"/>
+  </Link>
 }
