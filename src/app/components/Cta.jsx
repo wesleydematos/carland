2f678233090708d30737720ec91305aefe34189a
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-09-26 10:12:37 -0300

    fix: updated animation

--- src/app/components/Cta.jsx
+++ src/app/components/Cta.jsx
@@ -55,7 +55,7 @@ export default function Cta() {
             </div>
           </div>
           <motion.div 
-            variants={fadeIn("up", 0.8)}
+            variants={fadeIn("left", 0.8)}
             initial="hidden"
             whileInView={"show"}
             viewport={{once: false, amount: 0.2}}
